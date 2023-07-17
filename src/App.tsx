0109d2a0cd545b50d83f487db629b2cb9bf46f05
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-07-17 16:40:05 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
                 setCurrentStep={setCurrentStep}
                 userInfos={userInfos}
                 userPlan={userPlan}
-                userAddon={userAddons}
+                userAddons={userAddons}
               />
             }
           />
